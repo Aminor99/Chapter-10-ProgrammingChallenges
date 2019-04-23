1b86c1c89e152811acc0b2c44ae36cbeb5b3ce73
Asia Minor <asia.minor99@gmail.com>
2019-04-23 16:43:08 -0400

    Add files via upload

---
+++ CourseGrades.java
@@ -0,0 +1,16 @@
+//Name: Asia Minor
+//Date: 4-23-19
+//Purpose: Programming Challenge 5
+
+public class CourseGrades{
+
+   public static void main(String[] args){
+   
+      GradedActivity lab;
+      GradedActivity essay;
+      PassAllActivity activity; 
+      FinalExam finalE;
+      
+      GradedActivity grades[] = {lab, essay, activity, finalE};
+   }
+}
